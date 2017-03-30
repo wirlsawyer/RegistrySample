80e84addd5944176b28baf8a766b4dd69c1d8132
wirlsawyer <wirlsawyer@gmail.com>
2017-03-30 14:48:58 +0800

    First repository

---
+++ RegistrySample/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace RegistrySample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String _class = RegistryHelp.ReadRegistryKey(@"HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Class\{4D36E96C-E325-11CE-BFC1-08002BE10318}", "Class");
+            Console.WriteLine(_class);
+
+            IDictionary dict = new Dictionary<String, Object>();
+            RegistryHelp.ReadRegistryKey(@"HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Class\{4D36E96C-E325-11CE-BFC1-08002BE10318}", dict);
+            foreach (String key in dict.Keys)
+            {
+                Console.WriteLine("key:" + key + " Value:" + dict[key].ToString());
+            }
+        }
+    }
+}
---
+++ RegistrySample/RegistryHelp.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+using System.Collections;
+
+namespace RegistrySample
+{
+    class RegistryHelp
+    {
+        private const String HKEY_CLASSES_ROOT = "HKEY_CLASSES_ROOT\\";
+        private const String HKEY_CURRENT_USER = "HKEY_CURRENT_USER\\";
+        private const String HKEY_LOCAL_MACHINE = "HKEY_LOCAL_MACHINE\\";
+        private const String HKEY_USERS = "HKEY_USERS\\";
+        private const String HKEY_CURRENT_CONFIG = "HKEY_CURRENT_CONFIG\\";
+
+
+        static private RegistryKey GetKey(String RegPath)
+        {
+            RegistryKey RegKey = null;
+            try
+            {
+                if (RegPath.StartsWith(HKEY_CLASSES_ROOT))
+                {
+                    RegKey = Registry.ClassesRoot.OpenSubKey(RegPath.Substring(HKEY_CLASSES_ROOT.Length));
+                }
+                else if (RegPath.StartsWith(HKEY_CURRENT_USER))
+                {
+                    RegKey = Registry.CurrentUser.OpenSubKey(RegPath.Substring(HKEY_CURRENT_USER.Length));
+                }
+                else if (RegPath.StartsWith(HKEY_LOCAL_MACHINE))
+                {
+                    RegKey = Registry.LocalMachine.OpenSubKey(RegPath.Substring(HKEY_LOCAL_MACHINE.Length));
+                }
+                else if (RegPath.StartsWith(HKEY_USERS))
+                {
+                    RegKey = Registry.Users.OpenSubKey(RegPath.Substring(HKEY_USERS.Length));
+                }
+                else if (RegPath.StartsWith(HKEY_CURRENT_CONFIG))
+                {
+                    RegKey = Registry.CurrentConfig.OpenSubKey(RegPath.Substring(HKEY_CURRENT_CONFIG.Length));
+                }
+            }
+            catch (Exception e) { 
+            
+            }
+            
+            return RegKey;
+        }
+
+        static public String ReadRegistryKey(String RegPath, String Name)
+        {
+            RegistryKey RegKey = GetKey(RegPath);
+
+            try 
+            {
+                String RegValue = (String)RegKey.GetValue(Name);
+                return RegValue;
+            }
+            catch 
+            {
+                return null;
+            }
+        }
+
+        static public void ReadRegistryKey(String RegPath, IDictionary dict)
+        {
+            RegistryKey RegKey = GetKey(RegPath);
+            if (RegKey == null) return;
+
+            foreach (String Name in RegKey.GetValueNames())
+            {
+                RegistryValueKind RegKeyKind = RegKey.GetValueKind(Name);
+
+                switch (RegKeyKind)
+                {
+                    case RegistryValueKind.MultiString:
+                        String[] values = (String[])RegKey.GetValue(Name);
+                        if (Name.Length == 0)
+                        {
+                            dict.Add("(預設值)", String.Join(",", values));
+                        }
+                        else 
+                        {
+                            dict.Add(Name, String.Join(",", values));
+                        }                        
+                        break;
+
+                    case RegistryValueKind.String:
+                        if (Name.Length == 0)
+                        {
+                            dict.Add("(預設值)", (String)RegKey.GetValue(Name));
+                        }
+                        else
+                        {
+                            dict.Add(Name, (String)RegKey.GetValue(Name));
+                        }
+                        break;
+
+               
+                    
+                
+                    default:
+                        if (Name.Length == 0)
+                        {
+                            dict.Add("(預設值)", RegKey.GetValue(Name).ToString());
+                        }
+                        else
+                        {
+                            dict.Add(Name, RegKey.GetValue(Name).ToString());
+                        }
+                        break;
+                }
+            }
+
+            foreach (String SubName in RegKey.GetSubKeyNames())
+            {
+                IDictionary sub_dict = new Dictionary<String, Object>();
+                dict.Add(SubName, sub_dict);
+                ReadRegistryKey(RegPath + "\\" + SubName, sub_dict);
+                
+            }
+           
+        }
+    }
+}
